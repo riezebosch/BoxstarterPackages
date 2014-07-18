cc71b9ff9c6d927eadd3a43ad9946984bbfa12e6
Manuel Riezebosch <mriezebosch@gmail.com>
2014-07-18 12:03:04 +0200

    updateenabled=false toegevoegd aan sql server en management studio packages

--- MsSqlServer2014Express/tools/ChocolateyInstall.ps1
+++ MsSqlServer2014Express/tools/ChocolateyInstall.ps1
@@ -9,7 +9,7 @@ Write-Host "Extracting..."
 Start-Process "$exePath" "/Q /x:`"$extractPath`"" -Wait
 
 Write-Host "Installing..."
-& "$setupPath" /IACCEPTSQLSERVERLICENSETERMS /FEATURES=SQL /QS /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS
+& "$setupPath" /IACCEPTSQLSERVERLICENSETERMS /FEATURES=SQL /QS /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE
 
 Write-Host "Removing extracted files..."
 rm -r "$extractPath"
