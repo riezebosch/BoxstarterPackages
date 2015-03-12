73a1ebdbe13cd530684be26814e12f91aeb4e12a
Manuel Riezebosch <mriezebosch@gmail.com>
2015-03-12 12:27:52 +0100

    Merged 32bit and 64bit pacakges, modified download location to support caching.

--- MsSqlServer2014Express/tools/ChocolateyUninstall.ps1
+++ MsSqlServer2014Express/tools/ChocolateyUninstall.ps1
@@ -1,10 +1,15 @@
-$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$exePath = "$scriptPath\SQLEXPR_x64_ENU.exe"
-$extractPath = "$scriptPath\SQLEXPR_x64_ENU"
+$packageName = "MsSqlServer2014Express"
+$chocolateyTempDir = Join-Path $env:TEMP "chocolatey"
+$tempDir = Join-Path $chocolateyTempDir $packageName
+$fileFullPath = "$tempDir\SQLEXPR.exe"
+$extractPath = "$tempDir\SQLEXPR_ENU"
 $setupPath = "$extractPath\setup.exe"
 
+if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
+Get-ChocolateyWebFile $packageName $fileFullPath http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2032BIT/SQLEXPR_x86_ENU.exe http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2064BIT/SQLEXPR_x64_ENU.exe -validExitCodes @(0,-2067529716)
+
 Write-Host "Extracting..."
-Start-Process "$exePath" "/Q /x:`"$extractPath`"" -Wait
+Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
 
 Write-Host "Uninstalling..."
 & "$setupPath" /IACCEPTSQLSERVERLICENSETERMS /FEATURES=SQL /Q /ACTION=uninstall
--- MsSqlServerManagementStudio2014Express/tools/ChocolateyInstall.ps1
+++ MsSqlServerManagementStudio2014Express/tools/ChocolateyInstall.ps1
@@ -1,12 +1,15 @@
-$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$exePath = "$scriptPath\SQLManagementStudio_x64_ENU.exe"
-$extractPath = "$scriptPath\SQLManagementStudio_x64_ENU"
+$packageName = "MsSqlServerManagementStudio2014Express"
+$chocolateyTempDir = Join-Path $env:TEMP "chocolatey"
+$tempDir = Join-Path $chocolateyTempDir $packageName
+$fileFullPath = "$tempDir\SQLManagementStudio.exe"
+$extractPath = "$tempDir\SQLManagementStudio"
 $setupPath = "$extractPath\setup.exe"
- 
-Get-ChocolateyWebFile 'SQLManagementStudioExpress2014' "$exePath" http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2064BIT/SQLManagementStudio_x64_ENU.exe
+
+if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
+Get-ChocolateyWebFile $packageName $fileFullPath http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2032BIT/SQLManagementStudio_x86_ENU.exe http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2064BIT/SQLManagementStudio_x64_ENU.exe
  
 Write-Host "Extracting..."
-Start-Process "$exePath" "/Q /x:`"$extractPath`"" -Wait
+Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
 
 Write-Host "Installing..."
 & "$setupPath" /IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /UPDATEENABLED=FALSE
