69e9887348b4b891c281147fc92e08db2524d2bc
Manuel Riezebosch <mriezebosch@gmail.com>
2015-04-28 12:00:35 +0200

    Using Install-ChocolateyInstallPackage to support -ia and -o

--- MsSqlServer2014Express/tools/ChocolateyUninstall.ps1
+++ MsSqlServer2014Express/tools/ChocolateyUninstall.ps1
@@ -2,17 +2,21 @@ $packageName = "MsSqlServer2014Express"
 $chocolateyTempDir = Join-Path $env:TEMP "chocolatey"
 $tempDir = Join-Path $chocolateyTempDir $packageName
 $fileFullPath = "$tempDir\SQLEXPR.exe"
-$extractPath = "$tempDir\SQLEXPR_ENU"
+$extractPath = "$tempDir\SQLEXPR"
 $setupPath = "$extractPath\setup.exe"
+$silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /FEATURES=SQL /Q /ACTION=uninstall"
+$url = "http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2032BIT/SQLEXPR_x86_ENU.exe"
+$url64 = "http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2064BIT/SQLEXPR_x64_ENU.exe -validExitCodes @(0,-2067529716)"
 
+# Using the same download location as Install-ChocolateyPackage but need to create the directory first
 if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-Get-ChocolateyWebFile $packageName $fileFullPath http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2032BIT/SQLEXPR_x86_ENU.exe http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2064BIT/SQLEXPR_x64_ENU.exe -validExitCodes @(0,-2067529716)
+Get-ChocolateyWebFile $packageName $fileFullPath $url $url64
 
 Write-Host "Extracting..."
 Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
 
 Write-Host "Uninstalling..."
-& "$setupPath" /IACCEPTSQLSERVERLICENSETERMS /FEATURES=SQL /Q /ACTION=uninstall
+Uninstall-ChocolateyPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010)
 
 Write-Host "Removing extracted files..."
 rm -r "$extractPath"
--- MsSqlServerManagementStudio2014Express/tools/ChocolateyInstall.ps1
+++ MsSqlServerManagementStudio2014Express/tools/ChocolateyInstall.ps1
@@ -4,15 +4,19 @@ $tempDir = Join-Path $chocolateyTempDir $packageName
 $fileFullPath = "$tempDir\SQLManagementStudio.exe"
 $extractPath = "$tempDir\SQLManagementStudio"
 $setupPath = "$extractPath\setup.exe"
+$silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /UPDATEENABLED=FALSE"
+$url = "http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2032BIT/SQLManagementStudio_x86_ENU.exe"
+$url64 = "http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2064BIT/SQLManagementStudio_x64_ENU.exe"
 
+# Using the same download location as Install-ChocolateyPackage but need to create the directory first
 if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-Get-ChocolateyWebFile $packageName $fileFullPath http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2032BIT/SQLManagementStudio_x86_ENU.exe http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2064BIT/SQLManagementStudio_x64_ENU.exe
+Get-ChocolateyWebFile $packageName $fileFullPath $url $url64
  
 Write-Host "Extracting..."
 Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
 
 Write-Host "Installing..."
-& "$setupPath" /IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /UPDATEENABLED=FALSE
+Install-ChocolateyInstallPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010)
 
 Write-Host "Removing extracted files..."
-rm -r $extractPath
+rm -r "$extractPath"
