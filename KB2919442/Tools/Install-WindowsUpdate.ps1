008c3392cfebedf704d99f7834b31828e63399f8
Manuel Riezebosch <mriezebosch@gmail.com>
2015-09-18 16:24:12 +0200

    Using chocolatey functions for download

--- KB2919442/Tools/Install-WindowsUpdate.ps1
+++ KB2919442/Tools/Install-WindowsUpdate.ps1
@@ -6,14 +6,14 @@ function Install-WindowsUpdate([string]$kb, [Uri]$url, [string]$file, [string]$c
 		$out = Join-Path $temp $file
 		$extractPath = Join-Path $temp $kb
 		$packagePath = Join-Path $extractPath $cab
-		$logPath = Join-Path $extractPath "dism.log"
+		$logPath = Join-Path $temp "$kb.dism.log"
 
 		Write-Host("Downloading package...")
-		Invoke-WebRequest -Uri $url -OutFile $out
+		Get-ChocolateyWebFile $kb $out $url
 
 		Write-Host("Installing update $kb...")
 		&wusa $out /extract:$extractPath
-		&dism.exe /Online /Add-Package /PackagePath:$packagePath /NoRestart /Quiet /LogPath:$logPath
+		&dism.exe /Online /Add-Package /PackagePath:$packagePath /NoRestart /Quiet /LogPath:$logPath | Out-Null
 
 		if (Get-HotFix -id $kb)
 		{
