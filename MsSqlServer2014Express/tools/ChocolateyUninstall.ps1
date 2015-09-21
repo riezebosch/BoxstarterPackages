041e61a7d2fc8ebf5d6bf532b64b33723c0a8a3c
Manuel Riezebosch <mriezebosch@gmail.com>
2015-09-21 09:40:29 +0200

    fix for download url in uninstall script

--- MsSqlServer2014Express/tools/ChocolateyUninstall.ps1
+++ MsSqlServer2014Express/tools/ChocolateyUninstall.ps1
@@ -6,7 +6,7 @@ $extractPath = "$tempDir\SQLEXPR"
 $setupPath = "$extractPath\setup.exe"
 $silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /FEATURES=SQL /Q /ACTION=uninstall"
 $url = "http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2032BIT/SQLEXPR_x86_ENU.exe"
-$url64 = "http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2064BIT/SQLEXPR_x64_ENU.exe -validExitCodes @(0,-2067529716)"
+$url64 = "http://care.dlservice.microsoft.com/dl/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2064BIT/SQLEXPR_x64_ENU.exe"
 
 # Using the same download location as Install-ChocolateyPackage but need to create the directory first
 if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
