d00941154741a11377927a37f8a713918eee1f32
Manuel Riezebosch <mriezebosch@gmail.com>
2015-12-16 15:11:19 +0100

    relative path in layout

--- VisualStudio2015Enterprise/Tools/ChocolateyInstall.ps1
+++ VisualStudio2015Enterprise/Tools/ChocolateyInstall.ps1
@@ -6,14 +6,17 @@ $chocolateyTempDir = Join-Path $env:TEMP "chocolatey"
 $tempDir = Join-Path $chocolateyTempDir $packageName
 $fileFullPath = "$tempDir\vs_enterprise_ENU.exe"
 
+Write-Host "Parameters: $env:chocolateyPackageParameters"
+
 if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
 Get-ChocolateyWebFile $packageName $fileFullPath $url
 
-Write-Host "Parameters: $env:chocolateyPackageParameters"
-
 if ($env:chocolateyPackageParameters -match "(?<=/layout )\S+") {
 	$layout = $matches[0]
-	
+	if (!([System.IO.Path]::IsPathRooted($layout))) {
+		# Join relative path to package temp folder
+		$layout = Join-Path $tempDir $layout
+	}
 	# Create layout on specified location
 	Write-Host "Creating layout at: $layout"
 	Start-Process $fileFullPath "/layout $layout $params" -Wait
