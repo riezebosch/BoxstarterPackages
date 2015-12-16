1e492a353f7b0978c7eea10d658314de6f0563bb
Manuel Riezebosch <mriezebosch@gmail.com>
2015-12-16 14:42:27 +0100

    layout option as package parameter

--- VisualStudio2015Enterprise/Tools/ChocolateyInstall.ps1
+++ VisualStudio2015Enterprise/Tools/ChocolateyInstall.ps1
@@ -7,8 +7,19 @@ $tempDir = Join-Path $chocolateyTempDir $packageName
 $fileFullPath = "$tempDir\vs_enterprise_ENU.exe"
 
 if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-if (!(Test-Path $fileFullPath)) {
-	Get-ChocolateyWebFile $packageName $fileFullPath $url
+Get-ChocolateyWebFile $packageName $fileFullPath $url
+
+Write-Host "Parameters: $env:chocolateyPackageParameters"
+
+if ($env:chocolateyPackageParameters -match "(?<=/layout )\S+") {
+	$layout = $matches[0]
+	
+	# Create layout on specified location
+	Write-Host "Creating layout at: $layout"
+	Start-Process $fileFullPath "/layout $layout $params" -Wait
+	
+	# Redirect installation to layout folder
+	$fileFullPath = Join-Path $layout "vs_enterprise.exe"
 }
 
-Install-ChocolateyInstallPackage $packageName "exe" $params $fileFullPath -validExitCodes 0, 3010
+Install-ChocolateyInstallPackage $packageName "exe" $params $fileFullPath -validExitCodes @(0, 3010)
