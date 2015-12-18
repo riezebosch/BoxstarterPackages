bf99853a796b114c5b7179791fe7c258971ff63a
Manuel Riezebosch <mriezebosch@gmail.com>
2015-12-18 12:31:00 +0100

    valid exit code 3010

--- aspnet5/Tools/ChocolateyInstall.ps1
+++ aspnet5/Tools/ChocolateyInstall.ps1
@@ -1,4 +1,4 @@
 $packageName = "AspNet5.ENU.RC1_Update1"
 $params = "/install /passive /norestart /log $env:temp\aspnet.log"
 $url = "https://download.microsoft.com/download/B/0/A/B0AEBD7D-6979-4265-B1AC-A0B73618FB22/AspNet5.ENU.RC1_Update1.exe"
-Install-ChocolateyPackage $packageName "exe" $params $url
+Install-ChocolateyPackage $packageName "exe" $params $url -validExitCodes @(0, 3010)
