0ef48fcb70d55da65a4a5ea37a85d53ef6d119c6
Rami Abughazaleh <icnocop@noreply.users.github.com>
2015-12-22 22:48:40 -0800

    added support to the Windows8.1-KB2919355 and Windows8.1-KB2919442 chocolatey packages for Windows 8.1 x86 and Windows Server 2012 R2

--- KB2919355/Tools/ChocolateyInstall.ps1
+++ KB2919355/Tools/ChocolateyInstall.ps1
@@ -1,3 +1,35 @@
 . (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'Install-WindowsUpdate.ps1')
 
-Install-WindowsUpdate "KB2919355" 'http://download.microsoft.com/download/D/B/1/DB1F29FC-316D-481E-B435-1654BA185DCF/Windows8.1-KB2919355-x64.msu' "Windows8.1-KB2919355-x64.msu" "Windows8.1-KB2919355-x64.cab"
+$windowsVersion = (Get-CimInstance Win32_OperatingSystem).version
+$windowsProductType = (Get-WmiObject Win32_OperatingSystem).ProductType
+$windowsArchitecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+
+if (!(($windowsVersion -ge 6.3) -and ($windowsVersion -lt 6.4)))
+{
+	throw "Only Windows 8.1 and Winodws Server 2012 R2 are supported"
+}
+
+if ($windowsArchitecture -eq '64-bit')
+{
+	#64-bit
+	if ($windowsProductType -eq '1')
+	{
+		# Windows 8.1 x64
+		$url = "http://download.microsoft.com/download/D/B/1/DB1F29FC-316D-481E-B435-1654BA185DCF/"
+	}
+	else
+	{
+		# Windows Server 2012 R2
+		$url = "http://download.microsoft.com/download/2/5/6/256CCCFB-5341-4A8D-A277-8A81B21A1E35/"
+	}
+
+	$fileName = "Windows8.1-KB2919355-x64"
+}
+else
+{
+	# Windows 8.1 x86
+	$url = "http://download.microsoft.com/download/4/E/C/4EC66C83-1E15-43FD-B591-63FB7A1A5C04/"
+	$fileName = "Windows8.1-KB2919355-x86"
+}
+
+Install-WindowsUpdate "KB2919355" "$url$fileName.msu" "$fileName.msu" "$fileName.cab"
--- KB2919442/Tools/ChocolateyInstall.ps1
+++ KB2919442/Tools/ChocolateyInstall.ps1
@@ -1,3 +1,35 @@
 . (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'Install-WindowsUpdate.ps1')
 
-Install-WindowsUpdate "KB2919442" 'http://download.microsoft.com/download/C/F/8/CF821C31-38C7-4C5C-89BB-B283059269AF/Windows8.1-KB2919442-x64.msu' "Windows8.1-KB2919442-x64.msu" "Windows8.1-KB2919442-x64.cab"
+$windowsVersion = (Get-CimInstance Win32_OperatingSystem).version
+$windowsProductType = (Get-WmiObject Win32_OperatingSystem).ProductType
+$windowsArchitecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+
+if (!(($windowsVersion -ge 6.3) -and ($windowsVersion -lt 6.4)))
+{
+	throw "Only Windows 8.1 and Winodws Server 2012 R2 are supported"
+}
+
+if ($windowsArchitecture -eq '64-bit')
+{
+	#64-bit
+	if ($windowsProductType -eq '1')
+	{
+		# Windows 8.1 x64
+		$url = "http://download.microsoft.com/download/C/F/8/CF821C31-38C7-4C5C-89BB-B283059269AF/"
+	}
+	else
+	{
+		# Windows Server 2012 R2
+		$url = "http://download.microsoft.com/download/D/6/0/D60ED3E0-93A5-4505-8F6A-8D0A5DA16C8A/"
+	}
+
+	$fileName = "Windows8.1-KB2919442-x64"
+}
+else
+{
+	# Windows 8.1 x86
+	$url = "http://download.microsoft.com/download/9/D/A/9DA6C939-9E65-4681-BBBE-A8F73A5C116F/"
+	$fileName = "Windows8.1-KB2919442-x86"
+}
+
+Install-WindowsUpdate "KB2919442" "$url$fileName.msu" "$fileName.msu" "$fileName.cab"
