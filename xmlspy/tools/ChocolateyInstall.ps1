bfc1fac3c29e7c0d7a0c2ad21f599ba69dff3e57
Manuel Riezebosch <mriezebosch@gmail.com>
2016-04-28 12:18:01 +0200

    XMLSpy 2016

---
+++ xmlspy/tools/ChocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'xmlspy'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://cdn.sw.altova.com/v2016r2d/en/AuthenticEnt2016rel2.exe'
+$url64      = 'http://cdn.sw.altova.com/v2016r2d/en/AuthenticEnt2016rel2_x64.exe?file.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/qn+ /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'xmlspy*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
