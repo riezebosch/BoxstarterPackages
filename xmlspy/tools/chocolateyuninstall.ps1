71f6319d32a533301c1afd04280c3c7d39763255
Manuel Riezebosch <mriezebosch@gmail.com>
2016-05-25 15:24:52 +0200

    improved uninstaller, using the version number from the environment variable

--- xmlspy/tools/chocolateyuninstall.ps1
+++ xmlspy/tools/chocolateyuninstall.ps1
@@ -1,22 +1,26 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'xmlspy'
-$packageVersion = '2016.2.0'
-
-$chocolateyTempDir = Join-Path (Get-Item $env:TEMP).FullName "chocolatey"
-$tempDir = Join-Path $chocolateyTempDir -ChildPath $packageName | Join-Path -ChildPath $packageVersion
-
-$fileFullPath = Join-Path $tempDir "$($packageName)Install.EXE"
-
 $silentArgs = "/q /x"
 $url        = 'http://cdn.sw.altova.com/v2016r2d/en/XMLSpyEnt2016rel2.exe'
-$url64      = 'http://cdn.sw.altova.com/v2016r2d/en/XMLSpyEnt2016rel2_x64.exe'
+$url64bit   = 'http://cdn.sw.altova.com/v2016r2d/en/XMLSpyEnt2016rel2_x64.exe'
+$fileType   = "exe"
 
-# Using the same download location as Install-ChocolateyPackage but need to create the directory first
+# Resolving the download location in the same way as Install-ChocolateyPackage: 
+#   https://github.com/chocolatey/choco/blob/master/src/chocolatey.resources/helpers/functions/Install-ChocolateyPackage.ps1
+$chocTempDir = $env:TEMP
+$tempDir = Join-Path $chocTempDir "$packageName"
+if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageVersion"; }
+
 if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
+$file = Join-Path $tempDir "$($packageName)Install.$fileType"
 
-Get-ChocolateyWebFile $packageName $fileFullPath $url $url64
-Uninstall-ChocolateyPackage $packageName "EXE" $silentArgs $fileFullPath -validExitCodes @(0, 3010)
+$filePath = Get-ChocolateyWebFile $packageName $file $url $url64bit -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64 -options $options -getOriginalFileName
+
+# Supporting chocolatey 0.9.9.x where the original/full path was not returned by Get-ChocolateyWebFile
+if ($filePath -eq $null) { $filePath = $file }
+
+Uninstall-ChocolateyPackage $packageName "EXE" $silentArgs $filePath -validExitCodes @(0, 3010)
 
 
 
