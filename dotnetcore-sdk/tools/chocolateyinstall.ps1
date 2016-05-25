a74d948a36eb4e34d321514c1195e4e9b5010ecb
Manuel Riezebosch <mriezebosch@gmail.com>
2016-05-25 16:11:52 +0200

    dotnetcore runtime & sdk

---
+++ dotnetcore-sdk/tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'dotnetcore-sdk'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://go.microsoft.com/fwlink/?LinkID=798387'
+$url64      = 'https://go.microsoft.com/fwlink/?LinkID=798386'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/install /passive /norestart /log `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'dotnet-core*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
