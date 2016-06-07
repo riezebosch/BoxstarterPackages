76902b755b61421b5b788c1eaedf52bc55269382
Manuel Riezebosch <mriezebosch@gmail.com>
2016-06-07 13:30:56 +0200

    ms sql server 2016 express management studio

---
+++ MsSqlServerManagementStudio2016Express/tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'MsSqlServerManagementStudio2016Express'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download.microsoft.com/download/E/D/3/ED3B06EC-E4B5-40B3-B861-996B710A540C/SSMS-Setup-ENU.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /UPDATEENABLED=FALSE"
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'MsSqlServerManagementStudio2016Express*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MsSqlServerManagementStudio2016Express/tools/chocolateyuninstall.ps1
@@ -0,0 +1,48 @@
+﻿
+
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'mssqlservermanagementstudio2016express'
+$softwareName = 'Microsoft SQL Server Management Studio*' 
+$installerType = 'EXE' 
+
+$silentArgs = '/qn /norestart'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+if ($installerType -ne 'MSI') {
+  $silentArgs = ''
+  $validExitCodes = @(0)
+}
+
+$uninstalled = $false
+$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+
+$key = @(Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+                        -ErrorAction SilentlyContinue `
+         | ? { $_.DisplayName -like "$softwareName" })
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    if ("$($_.QuietUninstallString)" -match '"(.*)" (.*)') {
+      $file = $matches[1]
+      $silentArgs = $matches[2]
+    } 
+   
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
+
+
+
