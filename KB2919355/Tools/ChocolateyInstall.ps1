d2d3d7b7ce92609d9c15f6e8e285338b80c75be5
Manuel Riezebosch <mriezebosch@gmail.com>
2016-06-16 17:42:47 +0200

    Removed dots from Windows Update package id's

--- KB2919355/Tools/ChocolateyInstall.ps1
+++ KB2919355/Tools/ChocolateyInstall.ps1
@@ -10,27 +10,30 @@ if (!(($windowsVersion -ge [Version]'6.3') -and ($windowsVersion -lt [Version]'6
 	throw "Only Windows 8.1 and Windows Server 2012 R2 are supported"
 }
 
-# Windows 8.1 x86
-$url = "http://download.microsoft.com/download/4/E/C/4EC66C83-1E15-43FD-B591-63FB7A1A5C04/Windows8.1-KB2919355-x86.msu"
+if (!(Get-HotFix -id $kb -ea SilentlyContinue))
+{
+	# Windows 8.1 x86
+	$url = "http://download.microsoft.com/download/4/E/C/4EC66C83-1E15-43FD-B591-63FB7A1A5C04/Windows8.1-KB2919355-x86.msu"
 
-if ($windowsProductType -eq '1')
-{
-	# Windows 8.1 x64
-	$url64 = "http://download.microsoft.com/download/D/B/1/DB1F29FC-316D-481E-B435-1654BA185DCF/Windows8.1-KB2919355-x64.msu"
-}
-else
-{
-	# Windows Server 2012 R2
-	$url64 = "http://download.microsoft.com/download/2/5/6/256CCCFB-5341-4A8D-A277-8A81B21A1E35/Windows8.1-KB2919355-x64.msu"
-}
+	if ($windowsProductType -eq '1')
+	{
+		# Windows 8.1 x64
+		$url64 = "http://download.microsoft.com/download/D/B/1/DB1F29FC-316D-481E-B435-1654BA185DCF/Windows8.1-KB2919355-x64.msu"
+	}
+	else
+	{
+		# Windows Server 2012 R2
+		$url64 = "http://download.microsoft.com/download/2/5/6/256CCCFB-5341-4A8D-A277-8A81B21A1E35/Windows8.1-KB2919355-x64.msu"
+	}
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0, 3010)
+	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0, 3010)
 
-if (Get-HotFix -id $kb -ea SilentlyContinue)
-{
-	Write-Host("Install success.")
-}
-else
-{
-	throw "Hotfix still not applied after install."
+	if (Get-HotFix -id $kb -ea SilentlyContinue)
+	{
+		Write-Host("Install success.")
+	}
+	else
+	{
+		throw "Hotfix still not applied after install."
+	}
 }
