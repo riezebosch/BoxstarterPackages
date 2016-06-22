413428735fae5fa99acf011857d6b02310dad14f
Manuel Riezebosch <mriezebosch@gmail.com>
2016-06-22 14:40:36 +0200

    SQL Express 2016 with Advanced Services

---
+++ sql-server-express-adv/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'sql-server-express-adv'
+$url        = ''
+$url64      = 'https://download.microsoft.com/download/E/1/2/E12B3655-D817-49BA-B934-CEB9DAC0BAF3/SQLEXPRADV_x64_ENU.exe'
+$checksum   = '06F829DF0226F705758F9BB36D543622833C1ACE'
+$silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE"
+
+$chocTempDir =Join-Path (Get-Item $env:TEMP).FullName "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageVersion"; }
+
+if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
+$fileFullPath = "$tempDir\SQLEXPR.exe"
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileFullPath -Url $url -Url64bit $url64 -Checksum $checksum -ChecksumType 'sha1'
+
+Write-Host "Extracting..."
+$extractPath = "$tempDir\SQLEXPR"
+Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
+
+Write-Host "Installing..."
+$setupPath = "$extractPath\setup.exe"
+Install-ChocolateyInstallPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010)
+
+Write-Host "Removing extracted files..."
+Remove-Item -Recurse "$extractPath"
