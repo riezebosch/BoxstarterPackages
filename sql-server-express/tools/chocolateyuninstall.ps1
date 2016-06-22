d22237ddc49950ea7c45baf64ba924fb1ace9ec1
Manuel Riezebosch <mriezebosch@gmail.com>
2016-06-22 16:12:10 +0200

    removed the silent args for SetupARP because they get ignored anyway

--- sql-server-express/tools/chocolateyuninstall.ps1
+++ sql-server-express/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'sql-server-express'
-$silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /FEATURES=SQL /Q /ACTION=uninstall"
+$silentArgs = ""
 
 $setupPath = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft SQL Server SQLServer2016').UninstallString
 Uninstall-ChocolateyPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010)
