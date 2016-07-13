5ddf026a8fcb027bf3003b7003da3b694ff60bcb
Manuel Riezebosch <mriezebosch@gmail.com>
2016-07-13 13:51:51 +0200

    intellij 2016.2 added because choco package lags behind

---
+++ intellijidea-ultimate/tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+
+. $tools\uninstall.ps1
+
+Uninstall-ChocolateyPackage `
+  -PackageName 'intellijidea-ultimate' `
+  -FileType 'EXE' `
+  -Silent '/S' `
+  -File (Get-Uninstaller -Name 'IntelliJ IDEA Ultimate Edition 2016.1.1')
