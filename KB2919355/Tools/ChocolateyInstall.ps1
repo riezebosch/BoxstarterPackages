d12b3fa353a3899c16556da7d86f47f4c09c4191
Manuel Riezebosch <mriezebosch@gmail.com>
2016-07-13 15:25:20 +0200

    review comments on windows update packages do not fail on OS where package does not apply rename of package Id

--- KB2919355/Tools/ChocolateyInstall.ps1
+++ KB2919355/Tools/ChocolateyInstall.ps1
@@ -2,38 +2,33 @@ $kb = "KB2919355"
 $packageName = "KB2919355"
 $installerType = "msu"
 $silentArgs = "/quiet /norestart /log:`"$env:TEMP\$kb.Install.log`""
-$windowsVersion = [Version](Get-CimInstance Win32_OperatingSystem).Version
-$windowsProductType = (Get-CimInstance Win32_OperatingSystem).ProductType
 
-if (!(($windowsVersion -ge [Version]'6.3') -and ($windowsVersion -lt [Version]'6.4')))
-{
-	throw "Only Windows 8.1 and Windows Server 2012 R2 are supported"
+# Get-CimInstance only available on Powershell v3+ which is shipped since Windows 8 & Windows Server 2012
+$os = Get-CimInstance Win32_OperatingSystem -ea SilentlyContinue 
+$version = [Version]$os.Version
+
+if ($version -eq $null -or $version -lt [Version]'6.3' -or $version -ge [Version]'6.4') {
+	Write-Host "Skipping installation because this hotfix only applies to Windows 8.1 and Windows Server 2012 R2."
+	return
 }
 
-if (!(Get-HotFix -id $kb -ea SilentlyContinue))
+if (Get-HotFix -id $kb -ea SilentlyContinue)
 {
-	# Windows 8.1 x86
+	Write-Host "Skipping installation because hotfix $kb is already installed."
+	return
+}
+
+if ($os.ProductType -eq '1') {
+	# Windows 8.1
 	$url = "http://download.microsoft.com/download/4/E/C/4EC66C83-1E15-43FD-B591-63FB7A1A5C04/Windows8.1-KB2919355-x86.msu"
+	$url64 = "http://download.microsoft.com/download/D/B/1/DB1F29FC-316D-481E-B435-1654BA185DCF/Windows8.1-KB2919355-x64.msu"
+} else {
+	# Windows Server 2012 R2
+	$url64 = "http://download.microsoft.com/download/2/5/6/256CCCFB-5341-4A8D-A277-8A81B21A1E35/Windows8.1-KB2919355-x64.msu"
+}
 
-	if ($windowsProductType -eq '1')
-	{
-		# Windows 8.1 x64
-		$url64 = "http://download.microsoft.com/download/D/B/1/DB1F29FC-316D-481E-B435-1654BA185DCF/Windows8.1-KB2919355-x64.msu"
-	}
-	else
-	{
-		# Windows Server 2012 R2
-		$url64 = "http://download.microsoft.com/download/2/5/6/256CCCFB-5341-4A8D-A277-8A81B21A1E35/Windows8.1-KB2919355-x64.msu"
-	}
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0, 3010)
 
-	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0, 3010)
-
-	if (Get-HotFix -id $kb -ea SilentlyContinue)
-	{
-		Write-Host("Install success.")
-	}
-	else
-	{
-		throw "Hotfix still not applied after install."
-	}
+if (!(Get-HotFix -id $kb -ea SilentlyContinue)) {
+	throw "Hotfix still not found after installation was completed!"
 }
--- KB2919442/Tools/ChocolateyInstall.ps1
+++ KB2919442/Tools/ChocolateyInstall.ps1
@@ -2,41 +2,33 @@ $kb = "KB2919442"
 $packageName = "KB2919442"
 $installerType = "msu"
 $silentArgs = "/quiet /norestart /log:`"$env:TEMP\$kb.Install.log`""
-$windowsVersion = [Version](Get-CimInstance Win32_OperatingSystem).Version
-$windowsProductType = (Get-CimInstance Win32_OperatingSystem).ProductType
 
-if (!(($windowsVersion -ge [Version]'6.3') -and ($windowsVersion -lt [Version]'6.4')))
-{
-	throw "Only Windows 8.1 and Windows Server 2012 R2 are supported"
+# Get-CimInstance only available on Powershell v3+ which is shipped since Windows 8 & Windows Server 2012
+$os = Get-CimInstance Win32_OperatingSystem -ea SilentlyContinue 
+$version = [Version]$os.Version
+
+if ($version -eq $null -or $version -lt [Version]'6.3' -or $version -ge [Version]'6.4') {
+	Write-Host "Skipping installation because this hotfix only applies to Windows 8.1 and Windows Server 2012 R2."
+	return
 }
 
-if (!(Get-HotFix -id $kb -ea SilentlyContinue))
+if (Get-HotFix -id $kb -ea SilentlyContinue)
 {
-	# Windows 8.1 x86
+	Write-Host "Skipping installation because hotfix $kb is already installed."
+	return
+}
+
+if ($os.ProductType -eq '1') {
+	# Windows 8.1
 	$url = "http://download.microsoft.com/download/9/D/A/9DA6C939-9E65-4681-BBBE-A8F73A5C116F/Windows8.1-KB2919442-x86.msu"
-
-	if ($windowsProductType -eq '1')
-	{
-		# Windows 8.1 x64
-		$url64 = "http://download.microsoft.com/download/C/F/8/CF821C31-38C7-4C5C-89BB-B283059269AF/Windows8.1-KB2919442-x64.msu"
-	}
-	else
-	{
-		# Windows Server 2012 R2
-		$url64 = "http://download.microsoft.com/download/D/6/0/D60ED3E0-93A5-4505-8F6A-8D0A5DA16C8A/Windows8.1-KB2919442-x64.msu"
-	}
-
-	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0, 3010)
-
-	if (Get-HotFix -id $kb -ea SilentlyContinue)
-	{
-		Write-Host("Install success.")
-	}
-	else
-	{
-		throw "Hotfix still not applied after install."
-	}
+	$url64 = "http://download.microsoft.com/download/C/F/8/CF821C31-38C7-4C5C-89BB-B283059269AF/Windows8.1-KB2919442-x64.msu"
+} else {
+	# Windows Server 2012 R2
+	$url64 = "http://download.microsoft.com/download/D/6/0/D60ED3E0-93A5-4505-8F6A-8D0A5DA16C8A/Windows8.1-KB2919442-x64.msu"
 }
-else {
-	Write-Host "Hotfix already installed."
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0, 3010)
+
+if (!(Get-HotFix -id $kb -ea SilentlyContinue)) {
+	throw "Hotfix still not found after installation was completed!"
 }
