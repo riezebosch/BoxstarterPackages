1fa894b03d45a43f08e0b88fb3ae66e0f8ee13a8
Manuel Riezebosch <mriezebosch@gmail.com>
2016-08-15 13:31:22 +0200

    added checksum to kb's

--- KB2919355/Tools/ChocolateyInstall.ps1
+++ KB2919355/Tools/ChocolateyInstall.ps1
@@ -1,7 +1,6 @@
 $kb = "KB2919355"
 $packageName = "KB2919355"
-$installerType = "msu"
-$silentArgs = "/quiet /norestart /log:`"$env:TEMP\$kb.Install.log`""
+$silentArgs = "/quiet /norestart /log:`"$env:TEMP\$kb.Install.evt`""
 
 # Get-CimInstance only available on Powershell v3+ which is shipped since Windows 8 & Windows Server 2012
 $os = Get-CimInstance Win32_OperatingSystem -ea SilentlyContinue 
@@ -22,9 +21,12 @@ if ($os.ProductType -eq '1') {
 	# Windows 8.1
 	$url = "http://download.microsoft.com/download/4/E/C/4EC66C83-1E15-43FD-B591-63FB7A1A5C04/Windows8.1-KB2919355-x86.msu"
 	$url64 = "http://download.microsoft.com/download/D/B/1/DB1F29FC-316D-481E-B435-1654BA185DCF/Windows8.1-KB2919355-x64.msu"
+	$checksum = 'F8BECA5B463A36E1FEF45AD0DCA6A0DE7606930380514AC1852DF5CA6E3F6C1D'
+	$checksum64 = 'B0C9ADA530F5EE90BB962AFA9ED26218C582362315E13B1BA97E59767CB7825D'
 } else {
 	# Windows Server 2012 R2
 	$url64 = "http://download.microsoft.com/download/2/5/6/256CCCFB-5341-4A8D-A277-8A81B21A1E35/Windows8.1-KB2919355-x64.msu"
+	$checksum64 = 'B0C9ADA530F5EE90BB962AFA9ED26218C582362315E13B1BA97E59767CB7825D'
 }
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0, 3010)
+Install-ChocolateyPackage $packageName 'msu' $silentArgs $url $url64 -checksum $checksum -checksum64 $checksum64 -checksumType 'sha256' -validExitCodes @(0, 3010)
--- KB2919442/Tools/ChocolateyInstall.ps1
+++ KB2919442/Tools/ChocolateyInstall.ps1
@@ -1,7 +1,6 @@
 $kb = "KB2919442"
 $packageName = "KB2919442"
-$installerType = "msu"
-$silentArgs = "/quiet /norestart /log:`"$env:TEMP\$kb.Install.log`""
+$silentArgs = "/quiet /norestart /log:`"$env:TEMP\$kb.Install.evt`""
 
 # Get-CimInstance only available on Powershell v3+ which is shipped since Windows 8 & Windows Server 2012
 $os = Get-CimInstance Win32_OperatingSystem -ea SilentlyContinue 
@@ -22,9 +21,12 @@ if ($os.ProductType -eq '1') {
 	# Windows 8.1
 	$url = "http://download.microsoft.com/download/9/D/A/9DA6C939-9E65-4681-BBBE-A8F73A5C116F/Windows8.1-KB2919442-x86.msu"
 	$url64 = "http://download.microsoft.com/download/C/F/8/CF821C31-38C7-4C5C-89BB-B283059269AF/Windows8.1-KB2919442-x64.msu"
+	$checksum = '3368C3A329F402FD982B15B399368627B96973F008A5456B5286BDFC10C1169B'
+	$checksum64 = 'C10787E669B484674584A990E069295E8B81B5366F98508010A3AE181B729482'
 } else {
 	# Windows Server 2012 R2
 	$url64 = "http://download.microsoft.com/download/D/6/0/D60ED3E0-93A5-4505-8F6A-8D0A5DA16C8A/Windows8.1-KB2919442-x64.msu"
+	$checksum64 = 'C10787E669B484674584A990E069295E8B81B5366F98508010A3AE181B729482'
 }
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0, 3010)
+Install-ChocolateyPackage $packageName 'msu' $silentArgs $url $url64 -checksum $checksum -checksum64 $checksum64 -checksumType 'sha256' -validExitCodes @(0, 3010)
