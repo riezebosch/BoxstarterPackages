42b662a35f8c75a90a383232a874d0e0751dee08
Manuel Riezebosch <mriezebosch@gmail.com>
2016-08-16 09:49:45 +0200

    mindstorms NL package

---
+++ mindstorms-nl/tools/chocolateyinstall.ps1
@@ -0,0 +1,46 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'mindstorms-nl'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://esd.lego.com.edgesuite.net/digitaldelivery/mindstorms/6ecda7c2-1189-4816-b2dd-440e22d65814/public/LMS-EV3-WIN32-NL-01-02-01-full-setup.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'mindstorms*'
+
+  checksum      = '3B4759BF167DCFFE7A99B7D65557BBFA5656405394FFF05BFA5BDFE55DEE823B'
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'md5'
+
+  silentArgs    = "/q /acceptlicenses yes /r:n"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ mindstorms-nl/tools/chocolateyuninstall.ps1
@@ -0,0 +1,43 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'mindstorms-nl'
+$softwareName = 'LEGO MINDSTORMS EV3'
+$installerType = 'EXE' 
+
+$silentArgs = '/qb /x all'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+if ($installerType -ne 'MSI') {
+  $validExitCodes = @(0)
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName 
+	| Select-Object -First 1 # for some reason there are multiple keys with the software name
+
+if ($key.Count -eq 1) {
+  $key | % {
+	# slight modification to strip out the quotes and command line arguments
+	"$($_.UninstallString)" -match '"(.*)" (.*)'  
+    $file = $Matches[1]
+	$silentArgs = "$($Matches[2]) $silentArgs"
+
+    if ($installerType -eq 'MSI') {
+      $silentArgs = "$($_.PSChildName) $silentArgs"
+
+      $file = ''
+    }
+
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
