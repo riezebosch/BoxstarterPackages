85545e7951bd15127fa43c7c9b5c402d01e1f3b2
skyguy94 <skyguy94@gmail.com>
2016-09-25 08:22:22 -0500

    Enable users to specificy installer options. Set options to match UI defaults. Switch uninstaller to use installation media and allow for removal of the chocolatey created instance.

--- sql-server-express/tools/chocolateyinstall.ps1
+++ sql-server-express/tools/chocolateyinstall.ps1
@@ -1,26 +1,49 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿#These are the default UI settings
+$options = @{
+  instanceId = 'MSSQLSERVER'
+  instanceName = 'SQLExpress'
+  features = '"SQLEngine, Replication"'
+  updateEnabled = 'FALSE'
+}
 
-$packageName= 'sql-server-express'
-$url        = ''
-$url64      = 'https://download.microsoft.com/download/E/1/2/E12B3655-D817-49BA-B934-CEB9DAC0BAF3/SQLEXPR_x64_ENU.exe'
-$checksum   = '7D71A44096650E2916B921BE4EC3114EF95A8775'
-$silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE"
+$packageParameters = @{
+  packageName = 'sql-server-express';
+  url = '';
+  url64bit = 'https://download.microsoft.com/download/E/1/2/E12B3655-D817-49BA-B934-CEB9DAC0BAF3/SQLEXPR_x64_ENU.exe';
+  checksum = '';
+  checksumType = '';
+  checksum64 = '7D71A44096650E2916B921BE4EC3114EF95A8775';
+  checksumType64 = 'Sha1';
+  fileFullPath = '';
+}
 
-$tempDir = Join-Path (Get-Item $env:TEMP).FullName "$packageName"
-if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageVersion"; }
+if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+. "$PSScriptRoot\ChocolateyHelpers.ps1"
 
-if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-$fileFullPath = "$tempDir\SQLEXPR.exe"
+$tempFolder = Get-ChocolateyPackageTempFolder $packageParameters['packageName']
+$packageParameters['fileFullPath'] = "$tempFolder\SQLEXPR.exe"
+$extractPath = "$tempFolder\SQLEXPR"
 
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileFullPath -Url $url -Url64bit $url64 -Checksum $checksum -ChecksumType 'sha1'
+Get-ChocolateyWebFile @packageParameters
 
-Write-Host "Extracting..."
-$extractPath = "$tempDir\SQLEXPR"
-Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
+Start-Process $packageParameters['fileFullPath'] "/Q /x:`"$extractPath`"" -Wait | Out-Null
 
-Write-Host "Installing..."
-$setupPath = "$extractPath\setup.exe"
-Install-ChocolateyInstallPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010, 1116)
+$args = New-Object System.Collections.ArrayList
+$args.Add('/IACCEPTSQLSERVERLICENSETERMS') | Out-Null
+$args.Add('/Q') | Out-Null
+$args.Add('/ACTION=Install') | Out-Null
 
-Write-Host "Removing extracted files..."
-Remove-Item -Recurse "$extractPath"
+foreach($option in $options.GetEnumerator()) {
+  $key = $option.Key.ToUpper()
+  $value = $option.Value
+  if (!$value) { continue }
+  $args.Add("/$key=$value") | Out-Null
+}
+
+$joined = $args -join ' '
+Write-Host $joined
+Install-ChocolateyInstallPackage $packageParameters['packageName'] 'EXE' $joined "$extractPath\setup.exe" -validExitCodes @(0, 3010, 1116)
+
+Export-CliXml -Path (Join-Path $PSScriptRoot 'options.xml') -InputObject $options
+
+#Dont cleanup the media. Add or removing features and instances requires it.
