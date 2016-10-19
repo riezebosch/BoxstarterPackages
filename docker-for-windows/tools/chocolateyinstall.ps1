d2c76643299490a3e3b59d29f4dadca15eaeb4e3
Manuel Riezebosch <mriezebosch@gmail.com>
2016-10-19 23:04:41 +0200

    docker for windows 1.12.1

---
+++ docker-for-windows/tools/chocolateyinstall.ps1
@@ -0,0 +1,46 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'docker-for-windows'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.docker.com/win/stable/InstallDocker.msi'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'docker*'
+
+  checksum      = '89b9f231f292e8db433d3726fc92834a12eaa3f4aa474ace6352a13e486acb52'
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'md5'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
