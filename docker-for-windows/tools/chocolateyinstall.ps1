6c14b4503a0b63f63eb11b38311cc31038916981
Manuel Riezebosch <mriezebosch@gmail.com>
2016-11-02 09:15:46 +0100

    review comments on docker-for-windows

--- docker-for-windows/tools/chocolateyinstall.ps1
+++ docker-for-windows/tools/chocolateyinstall.ps1
@@ -1,11 +1,9 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 
-
 $packageName= 'docker-for-windows'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://download.docker.com/win/stable/InstallDocker.msi'
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $packageName
@@ -18,9 +16,7 @@ $packageArgs = @{
 
   checksum      = '89b9f231f292e8db433d3726fc92834a12eaa3f4aa474ace6352a13e486acb52'
   checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'md5'
-
+ 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
