b6046401a8baed04ef8c88f242daa35959481bb1
Manuel Riezebosch <mriezebosch@gmail.com>
2016-11-08 23:41:20 +0100

    SQL 2014 SP2 update

--- MsSqlServer2014Express/tools/ChocolateyInstall.ps1
+++ MsSqlServer2014Express/tools/ChocolateyInstall.ps1
@@ -5,14 +5,14 @@ $fileFullPath = "$tempDir\SQLEXPR.exe"
 $extractPath = "$tempDir\SQLEXPR"
 $setupPath = "$extractPath\setup.exe"
 $silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE"
-$url = "https://download.microsoft.com/download/1/5/6/156992E6-F7C7-4E55-833D-249BD2348138/ENU/x86/SQLEXPR_x86_ENU.exe"
-$url64 = "https://download.microsoft.com/download/1/5/6/156992E6-F7C7-4E55-833D-249BD2348138/ENU/x64/SQLEXPR_x64_ENU.exe"
-$checksum = '33C0112905B62B6BAD883112C2F49B50AA12C679'
-$checksum64 = '0C90C147A1C2A550165C9301AE7A6C604E318E51'
+$url = "https://download.microsoft.com/download/2/A/5/2A5260C3-4143-47D8-9823-E91BB0121F94/SQLEXPR_x86_ENU.exe"
+$url64 = "https://download.microsoft.com/download/2/A/5/2A5260C3-4143-47D8-9823-E91BB0121F94/SQLEXPR_x64_ENU.exe"
+$checksum = '0eff1354916410437c829e98989e5910d9605b2df31977bc33ca492405a0a9ab'
+$checksum64 = 'cc35e94030a24093a62e333e900c2e3c8f1eb253a5d73230a9f5527f1046825b'
 
 # Using the same download location as Install-ChocolateyPackage but need to create the directory first
 if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-Get-ChocolateyWebFile $packageName $fileFullPath $url $url64 -Checksum $checksum -Checksum64 $checksum64 -ChecksumType "sha1"
+Get-ChocolateyWebFile $packageName $fileFullPath $url $url64 -Checksum $checksum -Checksum64 $checksum64 -ChecksumType "sha256"
 
 Write-Host "Extracting..."
 Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
