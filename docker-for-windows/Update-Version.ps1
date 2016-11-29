9cbbba53b4dced7f47d63d2901341b176f643c25
Manuel Riezebosch <mriezebosch@gmail.com>
2016-11-29 10:01:54 +0100

    use docker specific version and update version from bootraspper intsaller

---
+++ docker-for-windows/Update-Version.ps1
@@ -0,0 +1,16 @@
+$MSIFile = "$env:TEMP\InstallDocker.msi"
+if (!(Test-Path $MSIFile)) {
+    (New-Object System.Net.WebClient).DownloadFile('https://download.docker.com/win/stable/InstallDocker.msi', $MSIFile)
+}
+
+. .\Get-DockerVersion.ps1
+$version = Get-DockerForWindowsVersion "$env:TEMP\InstallDocker.msi"
+
+$spec = "docker-for-windows.nuspec"
+(gc -Path $spec) -replace "<version>.+</version>", "<version>$version</version>" | Out-File $spec
+
+$install = ".\tools\chocolateyinstall.ps1"
+(gc -Path $install) -replace "\`$version = `".+`"", "`$version = `"$version`"" | Out-File $install
+
+$checksum = Invoke-WebRequest -Uri "https://download.docker.com/win/stable/$version/InstallDocker.msi.sha256sum" -UseBasicParsing
+(gc -Path $install) -replace "checksum      = '.+'", "checksum      = '$($checksum.Content.SubString(0, 64))'" | Out-File $install
