a9def44b6843722dd7690f5ffeaba77f3f51619a
Manuel Riezebosch <mriezebosch@gmail.com>
2016-11-30 17:36:47 +0100

    removed old packages

--- VisualStudio2015Enterprise/Tools/ChocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = "VisualStudio2015Enterprise"
-$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq "Microsoft Visual Studio Enterprise 2015" }
-if($app -ne $null){
-    $version=$app.Version
-    $uninstaller=Get-Childitem "$env:ProgramData\Package Cache\" -Recurse -Filter vs_enterprise.exe
-    Uninstall-ChocolateyPackage $packageName 'exe' "/Uninstall /force /Passive /NoRestart" $uninstaller.FullName
-}
