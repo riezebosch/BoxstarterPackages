a89ab0d579f6b14a6140fb7845585a8e1520d806
Manuel Riezebosch <mriezebosch@gmail.com>
2016-12-20 13:49:16 +0100

    using UTF8 for input and output

--- docker-for-windows/Update-Version.ps1
+++ docker-for-windows/Update-Version.ps1
@@ -1,16 +1,15 @@
 $MSIFile = "$env:TEMP\InstallDocker.msi"
-if (!(Test-Path $MSIFile)) {
-    (New-Object System.Net.WebClient).DownloadFile('https://download.docker.com/win/stable/InstallDocker.msi', $MSIFile)
-}
+(New-Object System.Net.WebClient).DownloadFile('https://download.docker.com/win/stable/InstallDocker.msi', $MSIFile)
 
 . .\Get-DockerVersion.ps1
 $version = Get-DockerForWindowsVersion "$env:TEMP\InstallDocker.msi"
+Write-Output "Version from msi: $version"
 
 $spec = "docker-for-windows.nuspec"
-(gc -Path $spec) -replace "<version>.+</version>", "<version>$version</version>" | Out-File $spec
+(gc -Path $spec -Encoding UTF8) -replace "<version>.+</version>", "<version>$version</version>" | Out-File $spec -Encoding utf8
 
 $install = ".\tools\chocolateyinstall.ps1"
-(gc -Path $install) -replace "\`$version = `".+`"", "`$version = `"$version`"" | Out-File $install
+(gc -Path $install) -replace "\`$version = `".+`"", "`$version = `"$version`"" | Out-File $install -Encoding utf8
 
 $checksum = Invoke-WebRequest -Uri "https://download.docker.com/win/stable/$version/InstallDocker.msi.sha256sum" -UseBasicParsing
-(gc -Path $install) -replace "checksum      = '.+'", "checksum      = '$($checksum.Content.SubString(0, 64))'" | Out-File $install
+(gc -Path $install) -replace "checksum      = '.+'", "checksum      = '$($checksum.Content.SubString(0, 64))'" | Out-File $install -Encoding utf8
