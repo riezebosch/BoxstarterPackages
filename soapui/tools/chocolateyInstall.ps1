b1d9a16e01abbf16a65eec9014995d4f02262cfd
Manuel Riezebosch <mriezebosch@gmail.com>
2016-12-27 12:18:55 +0100

    SoapUI 5.3.0 udpate

--- soapui/tools/chocolateyInstall.ps1
+++ soapui/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'soapui' # arbitrary name for the package, used in messages
 $fileType = 'exe'
-$url = 'http://cdn01.downloads.smartbear.com/soapui/5.2.1/SoapUI-x32-5.2.1.exe'
-$url64 = 'http://cdn01.downloads.smartbear.com/soapui/5.2.1/SoapUI-x64-5.2.1.exe'
-$checksum = '267D8120AF8B0A04FC195131C0B6A14775FF3850DC1C551F13B931C900F02773'
-$checksum64 = 'FCF8306D91B126CCB2A0DA82729F7481A74E71EF0045B6C5136D724F0D9C5ECA'
+$url = 'http://cdn01.downloads.smartbear.com/soapui/5.3.0/SoapUI-x32-5.3.0.exe'
+$url64 = 'http://cdn01.downloads.smartbear.com/soapui/5.3.0/SoapUI-x64-5.3.0.exe'
+$checksum = 'BA44B5EAF05472FF22C42CBED7C02BCF278A5A25B56842117A19ACE8EA69F6EE'
+$checksum64 = '73604FBECAD8A6022F084ED808A8D347CAA47E7B04CEC22E6782E91A6EA56634'
 
 Install-ChocolateyPackage $packageName $fileType '-q' $url $url64 -checksum $checksum -checksum64 $checksum64 -checksumType 'sha256'
