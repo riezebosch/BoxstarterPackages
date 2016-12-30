6efcf1b76cd75bf6bda48295d1403d2289d02b4d
Manuel Riezebosch <mriezebosch@gmail.com>
2016-12-30 22:01:22 +0100

    dotnetcore-sdk 1.0.3

--- dotnetcore-sdk/tools/chocolateyinstall.ps1
+++ dotnetcore-sdk/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'dotnetcore-sdk'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://go.microsoft.com/fwlink/?LinkID=798399'
-$url64      = 'https://go.microsoft.com/fwlink/?LinkID=798398'
+$url        = 'https://go.microsoft.com/fwlink/?LinkID=836287'
+$url64      = 'https://go.microsoft.com/fwlink/?LinkID=836281'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -18,10 +18,9 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'dotnet-core*'
-  checksum      = ''
-  checksumType  = 'md5'
-  checksum64    = ''
-  checksumType64= 'md5'
+  checksum      = '6BE9E4B34C168D65E51C9A4C05DCEBEF2CDE1FE07A1006A9777E427AB6D22D44'
+  checksumType  = 'sha256'
+  checksum64    = '60E4DDD00BF9620A00184D089C0ABAF118FF180A119FC1D5A1C512C4ACE41550'
 }
 
 Install-ChocolateyPackage @packageArgs
