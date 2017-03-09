f2cb210b78f465751d8f973f6643d21412bb0c2f
Manuel Riezebosch <mriezebosch@gmail.com>
2017-03-09 14:44:03 +0100

    vs2017 offline install package

---
+++ visualstudio2017-offline/tools/chocolateyinstall.ps1
@@ -0,0 +1,46 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'visualstudio2017-offline'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = "K:\VS2017\vs_Enterprise.exe"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  file         = $fileLocation
+
+  softwareName  = 'visualstudio2017-offline*'
+
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+
+  silentArgs    = "--passive --norestart --wait"
+  validExitCodes= @(0, 1, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
