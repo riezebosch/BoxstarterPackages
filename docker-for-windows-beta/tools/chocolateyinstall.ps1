b6627b1f9f699db39a6bb70cd8358f03fdb4f2f3
Manuel Riezebosch <mriezebosch@gmail.com>
2017-03-15 10:52:29 +0100

    docker-for-windows-beta static url

--- docker-for-windows-beta/tools/chocolateyinstall.ps1
+++ docker-for-windows-beta/tools/chocolateyinstall.ps1
@@ -1,9 +1,8 @@
-﻿$version = "1.13.1.10069"
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'docker-for-windows-beta'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "https://download.docker.com/win/beta/$version/InstallDocker.msi"
+$url        = "https://download.docker.com/win/beta/InstallDocker.msi"
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,7 +12,7 @@ $packageArgs = @{
 
   softwareName  = 'docker*'
 
-  checksum      = '9365A49FFCD496E9186181D4E84CF717C24D807EE28811FDB4872E6B6F0E0E26'
+  checksum      = 'ab41c8d50bfe302fdcf813428f984f8023c37298491788e35958a34e513554e1'
   checksumType  = 'sha256'
  
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
