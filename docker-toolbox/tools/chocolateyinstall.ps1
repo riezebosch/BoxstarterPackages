db6988dfc7d36b268ed18d3389c39bda8a2ad6ec
Manuel Riezebosch <mriezebosch@gmail.com>
2017-03-16 08:42:07 +0100

    docker toolbox

---
+++ docker-toolbox/tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'docker-toolbox'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.docker.com/win/stable/DockerToolbox.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'docker-toolbox*'
+
+  checksum      = 'DDE1714D951FC6FD7DFF7B63412A64083E1702F1B0707C3CD5F8BAEB797D7EA6'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/silent /norestart /log=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
