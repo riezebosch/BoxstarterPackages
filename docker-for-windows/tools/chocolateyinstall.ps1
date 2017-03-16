9c168f52d7f6993a6fc7b0377ff70520b762ccc2
Manuel Riezebosch <mriezebosch@gmail.com>
2017-03-16 11:11:35 +0100

    disable virtualbox in docker toolbox

--- docker-for-windows/tools/chocolateyinstall.ps1
+++ docker-for-windows/tools/chocolateyinstall.ps1
@@ -15,7 +15,7 @@ $packageArgs = @{
   checksum      = '8e7680db9ca049d31a0af71528d0bac507c4e8a52a87aa5c0b1d2a5eb83ef2e4'
   checksumType  = 'sha256'
  
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = "/COMPONENTS=`"docker,dockermachine,dockercompse`" /TASKS=`"modifypath,upgradevm`" /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
--- docker-toolbox/tools/chocolateyinstall.ps1
+++ docker-toolbox/tools/chocolateyinstall.ps1
@@ -18,7 +18,7 @@ $packageArgs = @{
   checksum      = 'DDE1714D951FC6FD7DFF7B63412A64083E1702F1B0707C3CD5F8BAEB797D7EA6'
   checksumType  = 'sha256'
 
-  silentArgs    = "/silent /norestart /log=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).log`""
+  silentArgs    = "/COMPONENTS=`"docker,dockermachine,dockercompose`" /TASKS=`"modifypath,upgradevm`" /silent /norestart /log=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
