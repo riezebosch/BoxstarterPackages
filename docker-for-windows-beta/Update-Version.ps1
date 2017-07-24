d0cada6d0b8d070cb4b4e8c2ef7e88043860d70c
Manuel Riezebosch <mriezebosch@gmail.com>
2017-07-24 10:11:24 +0200

    back to exe because of all update features etc

--- docker-for-windows-beta/Update-Version.ps1
+++ docker-for-windows-beta/Update-Version.ps1
@@ -1,15 +1,17 @@
 [xml]$data = iwr "https://download.docker.com/win/edge/appcast.xml"
+
 $match = $data.rss.channel.item.title | Select-String -Pattern "([0-9.]+).*\((\d+)\)"
 $version = "$($match.Matches.Groups[1]).$($match.Matches.Groups[2])"
-Write-Output "Version from installer: $version"
+$url = ($data | Select-Xml -XPath "//@d4w:url" -Namespace @{ d4w = "http://www.docker.com/docker-for-windows"  }).Node.Value
+$checksum = (iwr "$($url).sha256sum" -UseBasicParsing).Content.Split(' ')[0]
+
+Write-Output "Version: $version"
+Write-Host "Url: $url"
+Write-Host "Checksum: $checksum"
 
 $spec = "docker-for-windows.nuspec"
 (gc -Path $spec -Encoding UTF8) -replace "<version>.*</version>", "<version>$version-edge</version>" | Out-File $spec -Encoding utf8
 
-$url = $data.rss.channel.link
-$checksum = (iwr "$($url).sha256sum" -UseBasicParsing).Content.Split(' ')[0]
-
-Write-Host "Checksum: $checksum"
 $install = ".\tools\chocolateyinstall.ps1"
 (gc -Path $install) -replace "checksum      = '.*'", "checksum      = '$checksum'" | Out-File $install -Encoding utf8
 (gc -Path $install) -replace '\$url        = ''.*''', "`$url        = '$url'" | Out-File $install -Encoding utf8
--- docker-for-windows-beta/tools/chocolateyinstall.ps1
+++ docker-for-windows-beta/tools/chocolateyinstall.ps1
@@ -1,21 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= 'docker-for-windows-beta'
+$packageName= 'docker-for-windows-edge'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.docker.com/win/edge/12818/InstallDocker.msi'
+$url        = 'https://download.docker.com/win/edge/12818/Docker for Windows Installer.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'MSI'
+  fileType      = 'EXE'
   url           = $url
 
   softwareName  = 'docker*'
 
-  checksum      = '3a21ab331d68b4615719672360eeae2a257f4507abc43d078207aa33207a749b'
+  checksum      = '65d962a94ddfd0018ecceadb06c870c0ffdf6b6748aee31cacd8c087efe4743a'
   checksumType  = 'sha256'
  
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = "install --quiet"
   validExitCodes= @(0, 3010, 1641)
 }
 
