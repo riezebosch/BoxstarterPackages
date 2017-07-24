8aa4c930e45c1fdd4b42d7f656e75b32e643d54b
Manuel Riezebosch <mriezebosch@gmail.com>
2017-07-24 08:13:09 +0200

    dfw-edge using MSI again with static location and version number from RSS

--- docker-for-windows-beta/Update-Version.ps1
+++ docker-for-windows-beta/Update-Version.ps1
@@ -1,13 +1,15 @@
-$installer = "$env:TEMP\Docker for Windows Installer.exe"
-Start-BitsTransfer -Source https://download.docker.com/win/edge/Docker%20for%20Windows%20Installer.exe -Destination $installer
-$output = & $installer "check-for-update" | Select-String -Pattern "version=(\d+), displayVersion=([0-9.]+)"
-$version = "$($output.Matches.Groups[2]).$($output.Matches.Groups[1])"
-
+[xml]$data = iwr "https://download.docker.com/win/edge/appcast.xml"
+$match = $data.rss.channel.item.title | Select-String -Pattern "([0-9.]+).*\((\d+)\)"
+$version = "$($match.Matches.Groups[1]).$($match.Matches.Groups[2])"
 Write-Output "Version from installer: $version"
+
 $spec = "docker-for-windows.nuspec"
 (gc -Path $spec -Encoding UTF8) -replace "<version>.*</version>", "<version>$version-edge</version>" | Out-File $spec -Encoding utf8
 
-$checksum = & checksum -f $installer -t sha256
+$url = $data.rss.channel.link
+$checksum = (iwr "$($url).sha256sum" -UseBasicParsing).Content.Split(' ')[0]
+
 Write-Host "Checksum: $checksum"
 $install = ".\tools\chocolateyinstall.ps1"
 (gc -Path $install) -replace "checksum      = '.*'", "checksum      = '$checksum'" | Out-File $install -Encoding utf8
+(gc -Path $install) -replace '\$url        = ''.*''', "`$url        = '$url'" | Out-File $install -Encoding utf8
--- docker-for-windows-beta/tools/chocolateyinstall.ps1
+++ docker-for-windows-beta/tools/chocolateyinstall.ps1
@@ -2,20 +2,20 @@
 
 $packageName= 'docker-for-windows-beta'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "https://download.docker.com/win/edge/Docker%20for%20Windows%20Installer.exe"
+$url        = 'https://download.docker.com/win/edge/12818/InstallDocker.msi'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'MSI'
   url           = $url
 
   softwareName  = 'docker*'
 
-  checksum      = 'C9CA352161A94A21A5D2763781C82585A0DFFEF0AF9F367EF544213BD3C91346'
+  checksum      = '3a21ab331d68b4615719672360eeae2a257f4507abc43d078207aa33207a749b'
   checksumType  = 'sha256'
  
-  silentArgs    = "install --quiet"
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
