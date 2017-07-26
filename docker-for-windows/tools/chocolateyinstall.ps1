a12d9b85f55d62b93d0763be66550e1bcd7cb814
Manuel Riezebosch <mriezebosch@gmail.com>
2017-07-26 09:48:39 +0200

    d4w: updated nuspec and added AU for stable

--- docker-for-windows/tools/chocolateyinstall.ps1
+++ docker-for-windows/tools/chocolateyinstall.ps1
@@ -2,7 +2,8 @@
 
 $packageName= 'docker-for-windows'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "https://download.docker.com/win/stable/InstallDocker.msi"
+$url        = 'https://download.docker.com/win/stable/12801/InstallDocker.msi'
+$checksum   = '72a4c6456a4f055fae910e94a0b4c1dcca95ee765b1568981844308790ffccb6'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -12,7 +13,7 @@ $packageArgs = @{
 
   softwareName  = 'docker*'
 
-  checksum      = '72a4c6456a4f055fae910e94a0b4c1dcca95ee765b1568981844308790ffccb6'
+  checksum      = $checksum
   checksumType  = 'sha256'
  
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
