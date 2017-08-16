44b4ed2cdfc7a5a8a491b9085aa8e2b4cfe8b5da
Manuel Riezebosch <mriezebosch@gmail.com>
2017-08-16 08:54:46 +0200

    xmlspy: 2017 rel3 sp1

--- xmlspy/tools/ChocolateyInstall.ps1
+++ xmlspy/tools/ChocolateyInstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'xmlspy'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://cdn.sw.altova.com/v2017r3a/en/XMLSpyEnt2017rel3.exe?file.exe'
-$url64      = 'https://cdn.sw.altova.com/v2017r3a/en/XMLSpyEnt2017rel3_x64.exe?file.exe'
+$url        = 'https://cdn.sw.altova.com/v2017r3sp1a/en/XMLSpyEnt2017rel3sp1.exe'
+$url64      = 'https://cdn.sw.altova.com/v2017r3sp1a/en/XMLSpyEnt2017rel3sp1_x64.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -18,9 +18,9 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'xmlspy*'
-  checksum      = 'C27828EF87C1DCE7D07D66D52B21C019CE60852C250FBF98DC649A7F2F4D81B5'
+  checksum      = '220B5E81D29294FB854CBF70D5AC1D875C06A296F5272496C9055B880E52E19D'
   checksumType  = 'sha256'
-  checksum64    = '3B1D7C8D9624158D2142109904585851A4798BEDDE67EA28AD97B0BBF6401E39'
+  checksum64    = '374A3942D7158F3428C07279CBAF542F4254E620AA43DE92E1DFA6D471E6B0BA'
   checksumType64= 'sha256'
 }
 
