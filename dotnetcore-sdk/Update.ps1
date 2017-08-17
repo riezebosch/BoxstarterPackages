a9276008f6ba04e6c9de521ca63d31a19e21d1e5
Manuel Riezebosch <mriezebosch@gmail.com>
2017-08-17 16:15:13 +0200

    dotnetcore-sdk: url32/64 and checksum fix for AU

--- dotnetcore-sdk/Update.ps1
+++ dotnetcore-sdk/Update.ps1
@@ -5,26 +5,29 @@ $releases = "https://www.microsoft.com/net/download/core#/sdk"
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"           #1
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"           #1
             "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"      #2
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"           #1
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"      #2
+
         }
     }
 }
 
 function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases
-     $regex32 = 'dotnet-sdk.*-win-x86\.exe$'
-     $regex64 = 'dotnet-sdk.*-win-x64\.exe$'
 
-     $download_page.RawContent -match '(?<=<th>.NET Core\s*)\d+\.\d+(.\d){0,2}(?=\s*SDK</th>)'
+     # select <TH> containing SDK and extract version from it
+     ($download_page.AllElements | ? tagName -eq TH | ? innerHtml -match 'SDK').innerText -match '\d\.\d(\.d){0,2}'
      $version = $Matches[0]
 
+     # Add trailing .0 when version is only x.x
      if ($Matches.Count -eq 1) {
          $version = "$version.0"
      }
 
-     $url32 = $download_page.links | ? href -match $regex32 | select -First 1 -expand href
-     $url64 = $download_page.links | ? href -match $regex64 | select -First 1 -expand href
+     $url32 = ($download_page.links | ? href -match 'dotnet-sdk.*-win-x86\.exe$').href
+     $url64 = ($download_page.links | ? href -match 'dotnet-sdk.*-win-x64\.exe$').href
 
      return @{ Version = $version; URL32 = $url32; URL64 = $url64 }
 }
--- dotnetcore-sdk/tools/chocolateyinstall.ps1
+++ dotnetcore-sdk/tools/chocolateyinstall.ps1
@@ -1,11 +1,12 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 
-
 $packageName= 'dotnetcore-sdk'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://download.microsoft.com/download/0/F/D/0FD852A4-7EA1-4E2A-983A-0484AC19B92C/dotnet-sdk-2.0.0-win-x86.exe'
-$url64    = 'https://download.microsoft.com/download/0/F/D/0FD852A4-7EA1-4E2A-983A-0484AC19B92C/dotnet-sdk-2.0.0-win-x64.exe'
+$url        = 'https://download.microsoft.com/download/0/F/D/0FD852A4-7EA1-4E2A-983A-0484AC19B92C/dotnet-sdk-2.0.0-win-x86.exe'
+$checksum   = 'af488629001e60d50bd0ca268de28b2ab97f6c20213be007dda763b93d51c3d4'
+$url64      = 'https://download.microsoft.com/download/0/F/D/0FD852A4-7EA1-4E2A-983A-0484AC19B92C/dotnet-sdk-2.0.0-win-x64.exe'
+$checksum64 = 'cc490d28f55b67185688ff51dc6274aea6a582e47a07e9d084437e940c69f7a0'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -18,9 +19,9 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'dotnet-core*'
-  checksum      = 'af488629001e60d50bd0ca268de28b2ab97f6c20213be007dda763b93d51c3d4'
+  checksum      = $checksum
   checksumType  = 'sha256'
-  checksum64    = 'cc490d28f55b67185688ff51dc6274aea6a582e47a07e9d084437e940c69f7a0'
+  checksum64    = $checksum64
 }
 
 Install-ChocolateyPackage @packageArgs
