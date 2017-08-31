d7c0b25396661be1ca8c99401faac4f5ee759c3a
Chocolatey <chocolatey@realdimensions.net>
2017-08-31 00:10:05 +0000

    AU: 1 updated - docker-toolbox [skip ci] https://gist.github.com/78af415724ed3724701f732956900b2b/02ba80eb3963f5f8bb70a55acc71f5a8075b48b7

--- docker-toolbox/tools/chocolateyinstall.ps1
+++ docker-toolbox/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'docker-toolbox'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/docker/toolbox/releases/download/v17.06.0a-ce/DockerToolbox-17.06.0a-ce.exe'
-$checksum  =  '56eaf211f66b30c18de616d8bd710e377d153c65a340deaae791e10a61727b51'
+$url        = 'https://github.com/docker/toolbox/releases/download/v17.07.0-ce/DockerToolbox-17.07.0-ce.exe'
+$checksum  =  'e86c6a13f9147a5c108108fe4e917d189d8518c2069e7efdeedeb4529287823f'
 
 $packageArgs = @{
   packageName   = $packageName
