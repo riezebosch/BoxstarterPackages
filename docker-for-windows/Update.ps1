bd56f07e18f3a2a770d93f7824bab8e1234eea38
Manuel Riezebosch <mriezebosch@gmail.com>
2017-08-31 14:41:48 +0200

    dfw: stable switched to exe installer

--- docker-for-windows/Update.ps1
+++ docker-for-windows/Update.ps1
@@ -12,14 +12,14 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-     [xml]$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-     $regex = $download_page.rss.channel.item.title | Select-String -Pattern "([0-9.]+).*\((\d+)\)"
-     $version = "$($regex.Matches.Groups[1]).$($regex.Matches.Groups[2])"
+    [xml]$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regex = $download_page.rss.channel.item.title | Select-String -Pattern "([0-9.]+).*\((\d+)\)"
+    $version = "$($regex.Matches.Groups[1]).$($regex.Matches.Groups[2])"
 
-     # AU uses [Uri]::IsWellFormedUriString whereby spaces are not allowed in the Uri.
-     $url = ($download_page | Select-Xml -XPath "//@url").Node.Value -replace ' ', '%20'
-    
-     return @{ Version = $version; URL = $url }
+    # AU uses [Uri]::IsWellFormedUriString whereby spaces are not allowed in the Uri.
+    $url = ($download_page | Select-Xml -XPath "//@d4w:url" -Namespace @{ d4w = "http://www.docker.com/docker-for-windows"  }).Node.Value -replace ' ', '%20'
+   
+    return @{ Version = "$version"; URL = $url; PackageName = 'docker-for-windows' }
 }
 
 update
