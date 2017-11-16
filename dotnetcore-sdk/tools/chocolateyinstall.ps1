6dab4e6b570e64d83fc670bb56058318219e900a
Manuel Riezebosch <manuelriezebosch@nieuwe-macbook.local>
2017-11-16 10:06:30 +0100

    dotnetcore-sdk: 2.0.3

--- dotnetcore-sdk/tools/chocolateyinstall.ps1
+++ dotnetcore-sdk/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'dotnetcore-sdk'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/7/3/A/73A3E4DC-F019-47D1-9951-0453676E059B/dotnet-sdk-2.0.2-win-x86.exe'
-$checksum   = '352bf30e7d4339803cc5ded8d75de72acd26db7aabcc335d6b0fa10fc26719f6f001ec17b7f5bd296057004d8a2bc8e9d34bf47c40e1511dab2668cf4dc1d75b'
-$url64      = 'https://download.microsoft.com/download/7/3/A/73A3E4DC-F019-47D1-9951-0453676E059B/dotnet-sdk-2.0.2-win-x64.exe'
-$checksum64 = '0b3682c4c7b3b81906d3f011d8a45e06ec2470721a21693ad8040502f201008161f1d4ad73e05763386272c50bfe90be4e2a660734096b4d52e4997bb618a80c'
+$url        = 'https://download.microsoft.com/download/D/7/2/D725E47F-A4F1-4285-8935-A91AE2FCC06A/dotnet-sdk-2.0.3-win-x86.exe'
+$checksum   = '9933dbb2f52d3fe29e8825224e86016927d1fb791b0a5d36eea9efa5e30671e94a38497538c4dacb4059addcb100e89f803d2a8680d1c461128dd4efd4ff7161'
+$url64      = 'https://download.microsoft.com/download/D/7/2/D725E47F-A4F1-4285-8935-A91AE2FCC06A/dotnet-sdk-2.0.3-win-x64.exe'
+$checksum64 = '012f8530c2113c26c0205fe2d5497e6fec5823612b4249993eea20473a4d4cf5c35e1b2fd7133740981fdc8f23edc4caf315fb2b5a513429ad4ca77c7d11d619'
 
 $packageArgs = @{
   packageName   = $packageName
