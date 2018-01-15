e688c14d6dbb87353b919a46c6f91e40e56a6804
Manuel Riezebosch <mriezebosch@gmail.com>
2018-01-15 13:26:41 +0100

    AU for dotnetcore-sdk

--- dotnetcore-sdk/Update.ps1
+++ dotnetcore-sdk/Update.ps1
@@ -1,10 +1,6 @@
 Import-Module au
 
-$releases = "https://www.microsoft.com/net/download/core"
-
-$versionpattern = '(?<=<th>.*)\d\.\d(\.\d){0,2}(?=\s+SDK</th>)';        # <TH> with SDK in it
-$url32pattern = '(?<=href=[''"]).*dotnet-sdk.*-win-x86\.exe(?=[''"])'   # href with dotnet-sdk x86.exe
-$url64pattern = '(?<=href=[''"]).*dotnet-sdk.*-win-x64\.exe(?=[''"])'   # href with dotnet-sdk x64.exe
+$releases = "https://raw.githubusercontent.com/dotnet/core/master/release-notes/releases.json"
 
 function global:au_SearchReplace {
     @{
@@ -19,13 +15,13 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    # use basic parsing because of IE security issues: 
-    #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/382
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-     $version = ($download_page.RawContent | select-string -AllMatches $versionpattern).Matches.Value
-     $url32   = ($download_page.RawContent | select-string -AllMatches $url32pattern).Matches.Value     
-     $url64   = ($download_page.RawContent | select-string -AllMatches $url64pattern).Matches.Value     
+     $info = (Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json)[0]
+    
+     $exe32 = [System.IO.Path]::ChangeExtension($info.'sdk-win-x86', 'exe')
+     $exe64 = [System.IO.Path]::ChangeExtension($info.'sdk-win-x64', 'exe')
+     $version = $info.'version-sdk'
+     $url32   = $info.'dlc-sdk' + $exe32
+     $url64   = $info.'dlc-sdk' + $exe64
 
      return @{ Version = $version; URL32 = $url32; URL64 = $url64 }
 }
