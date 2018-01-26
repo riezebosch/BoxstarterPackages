8f34e1f1974585849e0f2ec7f0bb9cbc166a159e
Chocolatey <chocolatey@realdimensions.net>
2018-01-26 18:09:19 +0000

    AU: 1 updated - docker-for-windows [skip ci] https://gist.github.com/78af415724ed3724701f732956900b2b/9432dc45cc83c8de695cc9956a4cdb36830683b6

--- docker-for-windows-edge/tools/chocolateyinstall.ps1
+++ docker-for-windows-edge/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'docker-for-windows-edge'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.docker.com/win/edge/15285/Docker%20for%20Windows%20Installer.exe'
-$checksum   = 'a12a0b58c7aa7dce64e0353c969e980182cd08708924aa1aa574ab279dfb82f5'
+$url        = 'https://download.docker.com/win/edge/15489/Docker%20for%20Windows%20Installer.exe'
+$checksum   = 'e71bd369d8967ff685a56b9cdcfc651d4b4115801624d1202286c108302f7690'
 
 $packageArgs = @{
   packageName   = $packageName
