ae3a1968311d998bb564913da6892d1abbfa53c2
Chocolatey <chocolatey@realdimensions.net>
2018-08-29 12:30:17 +0000

    AU: 2 updated - docker-for-windows docker-for-windows [skip ci] https://gist.github.com/78af415724ed3724701f732956900b2b/3c8772ab1d7c255df5a0744a2b4e6cc7fde2f712

--- docker-for-windows-edge/tools/chocolateyinstall.ps1
+++ docker-for-windows-edge/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'docker-for-windows-edge'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.docker.com/win/edge/19101/Docker%20for%20Windows%20Installer.exe'
-$checksum   = '98807528412b06d737cf8eb1e79e7f3a7773985c7d2e5ee20de3384bc9f8acf1'
+$url        = 'https://download.docker.com/win/edge/19508/Docker%20for%20Windows%20Installer.exe'
+$checksum   = 'f4eb4a2be3bc08d0e2517daba4cce3299a764843719970f713a276d50797dd02'
 
 $packageArgs = @{
   packageName   = $packageName
--- docker-for-windows/tools/chocolateyinstall.ps1
+++ docker-for-windows/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'docker-for-windows'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.docker.com/win/stable/19098/Docker%20for%20Windows%20Installer.exe'
-$checksum   = '25ccc9ddc889de486fa0efc61d31a1c6a0c7bab94c890851d0e54fc1658c32ee'
+$url        = 'https://download.docker.com/win/stable/19507/Docker%20for%20Windows%20Installer.exe'
+$checksum   = '4f48ab087db0162bb5bc6df08222a1b61aefcfe06296cfee1cfc02d483ad11bd'
 
 $packageArgs = @{
   packageName   = $packageName
