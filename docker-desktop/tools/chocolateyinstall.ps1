74e4dd798f597971ded72443caf3a0df06a6a400
Chocolatey <chocolatey@realdimensions.net>
2019-02-06 12:10:10 +0000

    AU: 1 updated - docker-desktop [skip ci] https://gist.github.com/78af415724ed3724701f732956900b2b/d608e4065c9b14851cbfc019cf3c97a3ab69ae93

--- docker-desktop/tools/chocolateyinstall.ps1
+++ docker-desktop/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'docker-desktop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.docker.com/win/stable/30215/Docker%20for%20Windows%20Installer.exe'
-$checksum   = '9fdebb583c0b64b9fe1b66bbd17e5e0a28e36551587741f6b0188b7e8123a6b7'
+$url        = 'https://download.docker.com/win/edge/30972/Docker%20Desktop%20Installer.exe'
+$checksum   = 'f07860ccc0ac59bcaeb665a0386c87ab8dbbceb18e1e100f8f5fdf5adbe3b0e9'
 
 $packageArgs = @{
   packageName   = $packageName
