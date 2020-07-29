690ab9cf659d69b524ab89438bfa7c0b05546568
Romans Pokrovskis <motivated.it@gmail.com>
2020-07-29 10:40:35 +0300

    Update chocolateyInstall.ps1
    Added x64 5.6.0 as per https://www.soapui.org/downloads/latest-release/
    Even though official site links https://s3.amazonaws.com/downloads.eviware/soapuios/5.6.0/SoapUI-x32-5.6.0.exe the link seems broken so can't add x32 version.

--- soapui/tools/chocolateyInstall.ps1
+++ soapui/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://dl.eviware.com/soapuios/5.5.0/SoapUI-x32-5.5.0.exe'
-$url64      = 'http://dl.eviware.com/soapuios/5.5.0/SoapUI-x64-5.5.0.exe'
+$url64      = 'https://s3.amazonaws.com/downloads.eviware/soapuios/5.6.0/SoapUI-x64-5.6.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -14,7 +14,7 @@ $packageArgs = @{
 
   checksum      = '6f32e0f5415e759afcd88bbd19786d16263a36584d38373a54650298e53f84a0'
   checksumType  = 'sha256'
-  checksum64    = 'd88ef3a3cd312105ea7039e8e0ade21d31fe448f23872be69c29ffdbc912ae12'
+  checksum64    = 'ea0ecf663e68adbe346746fd2d8662bb713e124e8aa3b6cf769235c3f14e5210'
   checksumType64= 'sha256'
 
   silentArgs    = "-q"
