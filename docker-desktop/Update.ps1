c60cbe9b69934d4873b880f7d3e8e64821139d17
Manuel Riezebosch <mriezebosch@gmail.com>
2021-09-01 09:13:37 +0200

    feat(docker-desktop): cumulative stream

--- docker-desktop/Update.ps1
+++ docker-desktop/Update.ps1
@@ -9,28 +9,16 @@ function global:au_SearchReplace {
     }
 }
 
-function global:EntryToData($channel) {
-    $releases = "https://desktop.docker.com/win/$channel/amd64/appcast.xml"
+function global:au_GetLatest {
+    $releases = "https://desktop.docker.com/win/main/amd64/appcast.xml"
     [xml]$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $enclosure = $download_page | Select-Xml -XPath "/rss/channel/item/enclosure" | select -Last 1
     $version = ($enclosure | Select-Xml -XPath "@sparkle:shortVersionString" -Namespace @{ sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle" }).Node.Value
-    if ($channel -ne 'stable') {
-        $version += "-$channel"
-    }
 
     $url = ($enclosure | Select-Xml -XPath "@d4w:url" -Namespace @{ d4w = "http://www.docker.com/docker-for-windows"  }).Node.Value
 
     @{ Version = $version; URL = $url }
-}
-
-function global:au_GetLatest {
-      @{
-         Streams = [ordered] @{
-            'edge' = EntryToData('edge')
-            'main' = EntryToData('main')
-         }
-      }
 }
 
 update
