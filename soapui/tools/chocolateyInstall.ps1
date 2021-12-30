e1fb20d941023e8dc53d82e4b58a24a7ff86556a
4nd3r5 <arh@arh.dk>
2021-12-30 11:02:19 +0100

    Update chocolateyInstall.ps1

--- soapui/tools/chocolateyInstall.ps1
+++ soapui/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://dl.eviware.com/soapuios/5.5.0/SoapUI-x32-5.5.0.exe'
-$url64      = 'https://s3.amazonaws.com/downloads.eviware/soapuios/5.6.0/SoapUI-x64-5.6.0.exe'
+$url64      = 'https://s3.amazonaws.com/downloads.eviware/soapuios/5.6.1/SoapUI-x64-5.6.1.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -14,7 +14,7 @@ $packageArgs = @{
 
   checksum      = '6f32e0f5415e759afcd88bbd19786d16263a36584d38373a54650298e53f84a0'
   checksumType  = 'sha256'
-  checksum64    = 'ea0ecf663e68adbe346746fd2d8662bb713e124e8aa3b6cf769235c3f14e5210'
+  checksum64    = 'F32658CE0CD6BA22A9124B36C4A94543B49C25B3B9F5F54143374FCBC6ACA117'
   checksumType64= 'sha256'
 
   silentArgs    = "-q"
