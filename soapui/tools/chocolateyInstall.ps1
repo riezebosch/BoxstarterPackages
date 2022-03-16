43d2d7cf40d0f2c22bb603a429ea8fe94017eedb
OraDotNetDev <oradotnetdev@Outlook.com>
2022-03-16 19:01:12 +0000

    Remove references to 32bit payload as no longer available. Fail Package upgrade if 32bit version of app is installed

--- soapui/tools/chocolateyInstall.ps1
+++ soapui/tools/chocolateyInstall.ps1
@@ -1,25 +1,35 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop'
 $Version = "5.7.0"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "http://dl.eviware.com/soapuios/$Version/SoapUI-x32-$Version.exe"
 $url64      = "http://dl.eviware.com/soapuios/$Version/SoapUI-x64-$Version.exe"
+
+
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
   url64bit      = $url64
-
   softwareName  = 'soapui*'
-
-  checksum      = '6f32e0f5415e759afcd88bbd19786d16263a36584d38373a54650298e53f84a0'
-  checksumType  = 'sha256'
   checksum64    = '86B6E5658886A0876D0196394F91A470E1AF7D62D3067EEB94C1F8C5293D3B18'
   checksumType64= 'sha256'
-
   silentArgs    = "-q"
   validExitCodes= @(0, 3010, 1641)
+}
+
+$regKey = Get-UninstallRegistryKey  -SoftwareName $packageArgs['softwareName'] 
+
+if ($regKey)
+{
+  $regPath = $regKey | 
+                 Select-Object -ExpandProperty PSParentPath | 
+                 Convert-Path
+
+  # Throw terminating error if 32bit version of app is installed
+  if ($regPath -eq 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall')
+  {
+    Write-Error ( '"{0}" is installed, the final 32 bit version available is 5.5.0' -f $regKey.DisplayName  ) 
+  }
 }
 
 Install-ChocolateyPackage @packageArgs
