7c261079ed6becf045f6d81b76ba6f458a6e8962
bmcatherasoo <benaiahcatherasoo@gmail.com>
2022-06-21 09:40:23 +0200

    SQL Server Express Basic & Advanced Update to V15.0.2000.5

--- sql-server-express-adv/tools/chocolateyinstall.ps1
+++ sql-server-express-adv/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'sql-server-express-adv'
 $url        = ''
-$url64      = 'https://download.microsoft.com/download/E/1/2/E12B3655-D817-49BA-B934-CEB9DAC0BAF3/SQLEXPRADV_x64_ENU.exe'
-$checksum   = '06F829DF0226F705758F9BB36D543622833C1ACE'
+$url64      = 'https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLEXPRADV_x64_ENU.exe'
+$checksum   = '3BF3C226D2EBE9AEF3627E0E73A3A10903CFDEE49596D48A81FB80D495B2F72668E097C08F755599EE4F258D283CC97E0EB5E9701F3B092A6D2FFCB094543DA5'
 $silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE"
 
 $tempDir = Join-Path (Get-Item $env:TEMP).FullName "$packageName"
@@ -12,7 +12,7 @@ if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageV
 if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
 $fileFullPath = "$tempDir\SQLEXPR.exe"
 
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileFullPath -Url $url -Url64bit $url64 -Checksum $checksum -ChecksumType 'sha1'
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileFullPath -Url $url -Url64bit $url64 -Checksum $checksum -ChecksumType 'sha512'
 
 Write-Host "Extracting..."
 $extractPath = "$tempDir\SQLEXPR"
--- sql-server-express/tools/chocolateyinstall.ps1
+++ sql-server-express/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'sql-server-express'
 $url        = ''
-$url64      = 'https://download.microsoft.com/download/8/4/c/84c6c430-e0f5-476d-bf43-eaaa222a72e0/SQLEXPR_x64_ENU.exe'
-$checksum   = 'a0086387cd525be62f4d64af4654b2f4778bc98d'
+$url64      = 'https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLEXPR_x64_ENU.exe'
+$checksum   = 'B85F8A7F810B41AEA93783542E1F05631EAB65ED1E69DB48335CF14D4FE56E736AF99953C1533117E3EFF883FB42FBDCD36EBE99DA6F078FB41DC65FBA0D9128'
 $silentArgs = "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE"
 
 $tempDir = Join-Path (Get-Item $env:TEMP).FullName "$packageName"
@@ -12,7 +12,7 @@ if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageV
 if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
 $fileFullPath = "$tempDir\SQLEXPR.exe"
 
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileFullPath -Url $url -Url64bit $url64 -Checksum $checksum -ChecksumType 'sha1'
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileFullPath -Url $url -Url64bit $url64 -Checksum $checksum -ChecksumType 'sha512'
 
 Write-Host "Extracting..."
 $extractPath = "$tempDir\SQLEXPR"
