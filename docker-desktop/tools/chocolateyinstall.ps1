b12e132b276c7efe6b67a6e7a48601549c1923b5
Chocolatey <chocolatey@realdimensions.net>
2026-05-19 18:07:48 +0000

    AU: 1 updated - docker-desktop [skip ci]

--- docker-desktop/tools/chocolateyinstall.ps1
+++ docker-desktop/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'docker-desktop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://desktop.docker.com/win/main/amd64/226574/Docker%20Desktop%20Installer.exe'
-$checksum64 = '13a71ca029faa34947ffbf881bef63caee8094e5392e75ba57e420c78aacdf6b'
+$url64 = 'https://desktop.docker.com/win/main/amd64/227015/Docker%20Desktop%20Installer.exe'
+$checksum64 = 'f78111fae49b7ea099ec7af54caeae0395157f17a6e8cd01ec97565ba8f4600a'
 
 $packageArgs = @{
   packageName    = $packageName
